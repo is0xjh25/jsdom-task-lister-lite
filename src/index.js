a009103cf0ffaa8e6184b21c7d96a6222cd6773f
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-08 10:31:01 +0800

    Update: Todo list functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById("create-task-form");
+const list = document.getElementById("tasks");
+const input = document.getElementById("new-task-description");
+
+document.addEventListener("DOMContentLoaded", () => { 
+  form.addEventListener("submit", logSubmit);
 });
+
+function logSubmit(event) {
+  const content = document.createElement("li");
+  const deleteContent = document.createElement("button");
+  event.preventDefault();
+  content.textContent = input.value;
+  deleteContent.textContent = "X";
+  deleteContent.style.marginLeft = "5px";
+  deleteContent.setAttribute('data-description', input.value);
+  deleteContent.onclick = () => {
+    deleteContent.parentElement.remove();
+  };
+  content.appendChild(deleteContent);
+  list.appendChild(content);
+  input.value = null;
+}
